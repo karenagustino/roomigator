85b3f01c21d2584fe780ddff45ab21277ae3e847
jasperjjhe <jasperjiajie@gmail.com>
2025-10-05 04:32:42 -0700

    icon

--- room-finder/app/layout.tsx
+++ room-finder/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Roomigator",
   description: "Find any room. Instantly.",
+  icons: {
+    icon: "/icon.png",
+  }
 };
 
 export default function RootLayout({
